62eca605b3c7128edfca5b5caa1a5d1e0ef299b6
Desmends27 <desmondopokumends2004@gmail.com>
2023-12-25 20:07:34 +0000

    corrected format errors

--- script.js
+++ script.js
@@ -2,51 +2,56 @@ let button = document.querySelector('button');
 let complete = document.querySelector('.done');
 let form = document.querySelector('form');
 let cardNumber = document.querySelector('#cardNumber');
+let cardHolder = document.querySelector('#cardHolder');
 let inputs = document.getElementsByTagName('input');
 let inline_input = document.querySelector('.input_error');
 
 button.addEventListener('click', () => {
     checkInput(inputs, cardNumber);
-    let error_message = document.querySelector('.error-message');
-    if (error_message === null){
+    let error_messages = document.querySelectorAll('.error-message');
+    if (error_messages.length === 0){
         complete.classList.remove('hide');
         form.classList.add('hide');
     }
-  });
-  
-  function checkInput(inputs) {
+});
+
+function checkInput(inputs) {
     Array.from(inputs).forEach((input) => displayError(input));
-  }
-  
-  function displayError(input){
+}
+
+function displayError(input){
     let existingErrorMessage = input.parentNode.querySelector('.error-message');
     if (existingErrorMessage) {
-      existingErrorMessage.remove();
+        existingErrorMessage.remove();
     }
-  
     let errorMessage = document.createElement('p');
-    errorMessage.classList.add('error-message');
-  
+    errorMessage.style.color = 'red';
+    errorMessage.className = 'error-message';
+
     // Check if the input is blank
     if (input.value.trim().length === 0) {
-      errorMessage.innerHTML = 'No Blanks allowed';
-      if (input.id === 'year'  || input.id === 'month'){
-        // Clear existing content before appending
-        inline_input.innerHTML = '';
-        inline_input.appendChild(errorMessage);
-        console.log(inline_input)
-      }
-      else {
+        errorMessage.innerHTML = 'No Blanks allowed';
         input.parentNode.appendChild(errorMessage);
-      }
     }
-    else if (input.id === 'cardNumber' && containsLetter(cardNumber.value)){
-      errorMessage.innerHTML = 'Incorrect format, enter only numbers';
-      input.parentNode.appendChild(errorMessage);
+    else if (input.id === 'cardHolder' && !containsOnlyLetters(cardHolder.value.trim())){
+        errorMessage.innerHTML = 'Incorrect format, enter only letters';
+        input.parentNode.appendChild(errorMessage);
     }
-  }
-  
-  function containsLetter(str){
-    return /[a-zA-Z]/.test(str);
-  }
-  
+    else if (input.id !== 'cardHolder' && !containsNumbers(input.value.trim())){
+        errorMessage.innerHTML = 'Incorrect format, enter only numbers';
+        input.parentNode.appendChild(errorMessage);
+    }
+}
+
+function containsOnlyLetters(str) {
+    // Remove spaces from the string
+    const stringWithoutSpaces = str.replace(/\s/g, '');
+
+    // Check if the modified string contains only letters
+    return /^[a-zA-Z]+$/.test(stringWithoutSpaces);
+}
+
+function containsNumbers(str){
+    const stringWithoutSpaces = str.replace(/\s/g, '');
+    return /^[0-9]+$/.test(stringWithoutSpaces);
+}
